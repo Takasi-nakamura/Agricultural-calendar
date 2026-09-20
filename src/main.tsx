beb67a28030843946827ce860ca2c9f0b2895de0
Takasi-nakamura <haruto.yomogida@gmail.com>
2026-09-20 21:50:58 +0900

    Register service worker for PWA

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,18 @@
-import React from 'react'; import {createRoot} from 'react-dom/client'; import './style.css'; import App from './App'; createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style.css';
+import App from './App';
+
+function Root() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator && import.meta.env.PROD) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register(`${import.meta.env.BASE_URL}sw.js`).catch(() => {});
+      });
+    }
+  }, []);
+
+  return <React.StrictMode><App /></React.StrictMode>;
+}
+
+createRoot(document.getElementById('root')!).render(<Root />);
